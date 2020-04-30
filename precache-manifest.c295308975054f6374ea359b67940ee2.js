5c250f47f91a5148a3b76d57c60e24232ee02eca
Jonathan Tse <Jonathan@Jonathans-MBP.connect>
2020-04-30 21:37:29 +0100

    Updates

---
+++ precache-manifest.c295308975054f6374ea359b67940ee2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd539385e292769132c7b911b71173cd",
+    "url": "/weather-api/index.html"
+  },
+  {
+    "revision": "792d7bd6ea262e8bbb7a",
+    "url": "/weather-api/static/css/main.6226ae08.chunk.css"
+  },
+  {
+    "revision": "9243b8a5c18f989b76c2",
+    "url": "/weather-api/static/js/2.9a564642.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-api/static/js/2.9a564642.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "792d7bd6ea262e8bbb7a",
+    "url": "/weather-api/static/js/main.d24c1494.chunk.js"
+  },
+  {
+    "revision": "41fb2821e4d953de42b8",
+    "url": "/weather-api/static/js/runtime-main.944db050.js"
+  }
+]);
